e4d74caf29a55226e00ecc5952dd8f519ea32251
iceyang <w@icey.cc>
2019-11-25 20:12:40 +0800

    es6

--- 4.es6/src/index.js
+++ 4.es6/src/index.js
@@ -5,14 +5,15 @@
 // 装饰类  ：可以在类上 扩展一些方法 或者属性
 @add
 class My{
-    @enumerable PI = 3.14; // 不可枚举
+    // @enumerable PI = 3.14; // 不可枚举
     @readonly r = 3.15
 }
 function add(target){
     target.type = 'my'
 }
+console.log(My.type)
 function readonly(target,key,descriptor){// Object.defineproperty(obj,pi,{})
-    // console.log(target,key,descriptor.initializer());
+    console.log(target,key,descriptor);
     descriptor.writable = false;
     // setTimeout(()=>{
     //     console.log(My.prototype === target);
@@ -29,45 +30,45 @@ for(let key in my){
 
 
 
-class Animal{
-    @enumerable
-    @readonly PI = 3.4
-    @beforeSay
-    say(){
-        console.log('say')
-    }
-}
+// class Animal{
+//     @enumerable
+//     @readonly PI = 3.4
+//     @beforeSay
+//     say(){
+//         console.log('say')
+//     }
+// }
 
-function beforeSay(target,key,descriptor){
-    let oldSay = descriptor.value
-    descriptor.value = function(){
-        console.log('before say')
-        oldSay.call(this)
-    }
-}
-let animal = new Animal();
-for(let key in animal){
-    console.log(key);
-}
+// function beforeSay(target,key,descriptor){
+//     let oldSay = descriptor.value
+//     descriptor.value = function(){
+//         console.log('before say')
+//         oldSay.call(this)
+//     }
+// }
+// let animal = new Animal();
+// for(let key in animal){
+//     console.log(key);
+// }
 
-// 装饰器多个 时候怎么执行
-@logger2()
-@logger1()
-class Logger{
+// // 装饰器多个 时候怎么执行
+// @logger2()
+// @logger1()
+// class Logger{
 
-}
-function logger1(){
-    console.log('outter 1')
-   return function(){
-        console.log('logger1');
-   }
-}   
-function logger2(){
-    console.log('outter 2')
-    return function(){
-        console.log('logger2');
-    }
-}
+// }
+// function logger1(){
+//     console.log('outter 1')
+//    return function(){
+//         console.log('logger1');
+//    }
+// }   
+// function logger2(){
+//     console.log('outter 2')
+//     return function(){
+//         console.log('logger2');
+//     }
+// }
 // 洋葱模型
 // 如果装饰器是函数 会直接执行，内部装饰类的时候会从内部依次传递到外部去
 
