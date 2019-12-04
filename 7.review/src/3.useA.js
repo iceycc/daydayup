64d2d9f9a59b922b75806bf47ebf066c82a39b38
iceyang <w@icey.cc>
2019-12-05 00:11:59 +0800

    update

--- 7.review/src/3.useA.js
+++ 7.review/src/3.useA.js
@@ -3,15 +3,17 @@
 // console.log(r);
 
 // 模块的分类
-// 自己写的模块 require('../') 绝对路径 文件模块
-// 核心模块 require('fs') 没有路径 不用安装
-// 第三方模块 需要安装 用法和核心模块是一样的,会去当前目录下查找
+// 1 自己写的模块 require('../') 绝对路径 文件模块
+// 2 核心模块 require('fs') 没有路径 不用安装
+// 3 第三方模块 需要安装 用法和核心模块是一样的,会去当前目录下查找
 
-// 第三方模块的查找规则
-// 包 package.json 找不到才会找index.js
+// 第三方模块的查找规则：node_modules
+// 先着包 package.json 找不到才会找index.js
 // 会像上级查找  每个node 版本会有差异
-
-// 名字和文件夹相同 会找文件 ，如果没文件找文件夹，默认找index,如果写了ackage.json => main 找不到报错
+// 路径 modules.paths // 
+// 名字和文件夹相同 会找文件 ，
+// 如果没文件找文件夹，默认找index,
+// 如果写了ackage.json => main 找不到报错
 require('./a');
 
 
@@ -19,6 +21,18 @@ require('./a');
 
 
 // npm init -y
+// package.json 下的 
+// script脚本的执行顺序：
+// 默认找 node_modules .bin 下的文件
+// npx 也可以直接执行 .bin 目录下的目录
+// npx mime a.js // 先找.bin下，没有的话会临时安装包 并执行，执行玩会直接删除.用完一次就删除
+// 类比全局安装，命令行任何位置执行
+
+// #! /usr/bin/env node
+
+
+
+
 
 
 // 周五 buffer fs 递归文件
--- 7.review/src/a1.js
+++ 7.review/src/a1.js
@@ -5,8 +5,20 @@
 // exports.a= 'hello';
 module.exports = 'hello';
 console.log('file')
-
+console.log(this)
+console.log(module)
+console.log(exports === module.exports) // true
 // 错误写法 exports = 123;
 
 
+
+// 内部实现：
+// expors = module.exports = {}
+// return module.exports
+
+// 三种导出的方式
+// exports = 'hellow' //错误写法：这么无法改变module.exports
+exports.a = 'abc'  // => 改变后   module.exports = {a:'abc'}
+module.exports = 'abc'  // => 改变后   module.exports = {a:'abc'}
+global.a = 123; // 污染全局变量
 
