6b8a22a82c8d3e91efec98aa9db463a52c77e364
iceyang <w@icey.cc>
2019-12-30 20:30:39 +0800

    a

--- 13.http/4.gzip.js
+++ 13.http/4.gzip.js
@@ -1,7 +1,12 @@
-// 压缩 转化流
+/**
+ * 压缩 转化流
+ * Accept-Encoding：请求头内的 客户端告诉服务器支持的压缩格式
+ * content-encoding ： 响应头内的 服务器告诉客户端，内容是用什么格式压缩的
+ */
 const zlib = require('zlib');
-
-// .gz
+// .gz 
+// 压缩zlib.gzip()
+// 解压zlib.unzip()
 const fs = require('fs');
 // let content = fs.readFileSync('./a.txt')
 // zlib.gzip(content,function(err,data){
--- 13.http/5.user-agent.js
+++ 13.http/5.user-agent.js
@@ -1,18 +1,24 @@
+// 根据客户端的user-agent，设置不同的逻辑
+// 模拟请求：
+// curl --header "User-Agent:Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1" http://localhost:5000
+
+// 前后端都可以做
 const http = require('http');
 
 http.createServer((req,res)=>{
     let agent = req.headers['user-agent'];
     if(agent.match('iPhone')){
         res.statusCode = 302;
+        console.log('iPhone')
         res.setHeader('Location','http://www.baidu.com')
         res.end(); // 阻止当前页面 redirect
     }else{
+        console.log('not iPhone')
         res.statusCode = 302;
         res.setHeader('Location','http://www.qq.com')
         res.end(); // 阻止当前页面 redirect
     }
 }).listen(5000);
-
 
 // Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1
 
