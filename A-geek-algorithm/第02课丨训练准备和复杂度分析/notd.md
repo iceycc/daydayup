ff06e7fcc417420c7bd663311b8ec12611dcf9e8
iceyang <w@icey.cc>
2020-03-07 17:55:19 +0800

    up

--- A-geek-algorithm/第02课丨训练准备和复杂度分析/notd.md
+++ A-geek-algorithm/第02课丨训练准备和复杂度分析/notd.md
@@ -7,7 +7,10 @@
 - 自顶向下编程的 LeetCode 例题 http://leetcode-cn.com/problems/valid-palindrome/
 
 ## Course 2 时间复杂度和空间复杂度分析
-- 如何理解算法时间复杂度的表示法 www.zhihu.com/question/21387264
+- 如何理解算法时间复杂度的表示法 https://www.zhihu.com/question/21387264
 - Master theorem en.wikipedia.org/wiki/Master_theorem_(analysis_of_algorithms)
 - 主定理 zh.wikipedia.org/wiki/%E4%B8%BB%E5%AE%9A%E7%90%86
 
+
+## 时间和空间复杂度
+Big O notation
--- D-python-2020/week1/4.sockerserver.py
+++ D-python-2020/week1/4.sockerserver.py
@@ -1,14 +1,14 @@
 import socket
-HOST = '192.168.0.112'
-PORT = 80
+HOST = '192.168.0.116'
+PORT = 8080
 # 当作文件 with
 # 将豆瓣网想象为放在网上的文件
 # TCP协议
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
   s.bind((HOST,PORT))
   s.listen(1)
-  conn,addr = s.accept()
-  with conn:
+  conn,addr = s.accept() # 端口可以接受请求了
+  with conn: # 
     print(f'Connected from {addr}')
     while True:
       data = conn.recv(1024) # 接收的缓冲区
