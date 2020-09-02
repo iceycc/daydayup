2db1eb129770d5fcaa6fe9d635574ede7422626a
wangbingyang <wangbingyang@doushen.com>
2020-09-02 09:05:44 +0800

    新增user模块

--- 03-nestJs/test1/src/post/post.controller.spec.ts
+++ 03-nestJs/test1/src/post/post.controller.spec.ts
@@ -1,15 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { UserController } from './user.controller';
+import { PostController } from './post.controller';
 
-describe('UserController', () => {
-  let controller: UserController;
+describe('PostController', () => {
+  let controller: PostController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [UserController],
+      controllers: [PostController],
     }).compile();
 
-    controller = module.get<UserController>(UserController);
+    controller = module.get<PostController>(PostController);
   });
 
   it('should be defined', () => {
--- 03-nestJs/test1/src/user/user.module.ts
+++ 03-nestJs/test1/src/user/user.module.ts
@@ -1,7 +1,29 @@
-import { Module } from '@nestjs/common';
-import { UserService } from './user.service';
+import { Module, Get, Query, Post, Body ,Response, Patch, Delete} from '@nestjs/common';
+import { query } from 'express';
 
-@Module({
-  providers: [UserService]
-})
-export class UserModule {}
+@Module({})
+export class UserModule {
+    @Get()
+    getUser(@Query() query:number){
+        if(query){
+            return [{id:query,name:'wby'}]
+        }else{
+            return [{id:query,name:'wby'},{id:2,name:'bbbbss'}]
+        }
+    }
+    @Post()
+    addUser(@Body() body:any,@Response() res:any){
+        console.log(body)
+        res.send('添加用户成功')
+    }
+    @Patch()
+    updateUser(@Body() body:any,@Response() res:any){
+        console.log(body)
+        res.send('更新用户成功')
+    }
+    @Delete()
+    deleteUser(@Body() body:any,@Response() res:any){
+        console.log(body)
+        res.send('删除用户成功')
+    }
+}
