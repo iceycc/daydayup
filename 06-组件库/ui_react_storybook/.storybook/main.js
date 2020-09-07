fdcd3b237187443064a00897d75c2da46dbbf6f4
iceyang <w@icey.cc>
2020-09-07 09:10:02 +0800

    add react组件库by storybook

---
+++ 06-组件库/ui_react_storybook/.storybook/main.js
@@ -0,0 +1,27 @@
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-storysource",
+    "@storybook/addon-viewport",
+    "@storybook/addon-docs",
+    "@storybook/addon-knobs"
+  ],
+  webpackFinal: async (config) => {
+		config.module.rules.push({
+			test: /\\.(ts|tsx)$/,
+			use: [
+				{
+					loader: require.resolve("react-docgen-typescript-loader"),
+				},
+			],
+		});
+		config.resolve.extensions.push(".ts", ".tsx");
+		return config;
+	},
+}
