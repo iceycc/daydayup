e4cb01d17bac393f164b3f1aaeca2d6bb10095af
wangbingyang <wangbingyang@doushen.com>
2021-04-09 20:32:32 +0800

    update vue

--- 11-vue/vue2-demo/src/main.js
+++ 11-vue/vue2-demo/src/main.js
@@ -1,12 +1,30 @@
 import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+// import App from './App.vue'
+// import router from './router'
+// import store from './store'
 
 Vue.config.productionTip = false
+// debugger
+// new Vue({
+//   router,
+//   store,
+//   render: h => h(App)
+// }).$mount('#app')
+// debugger
 debugger
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+  el:'#app',
+  render(c){
+    return c('div',{
+      attrs:{
+        id:'app'
+      }
+    },this.message)
+  },
+  data: {
+    message: 'Hello!'
+  },
+  created(){
+    // console.log(aa)
+  }
+})
--- 11-vue/vue2-demo/vue.config.js
+++ 11-vue/vue2-demo/vue.config.js
@@ -11,5 +11,8 @@ function resolve(dir){
   },
   chainWebpack(config){
     // config.resolve.alias.set('vue',resolve('vue/vue.esm.js'))
-  }
+  },
+  runtimeCompiler: true // 
+  // 如果在之后的开发中，你依然使用template，就需要选择runtimecompiler
+  // 如果你之后的开发中，使用的是.vue文件夹开发，那么可以选择runtimeonly
 }
