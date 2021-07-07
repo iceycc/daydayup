52380dde9906f3aa8a5ad9b664fc15ec45391d7b
wangbingyang <wangbingyang@doushen.com>
2021-07-07 20:54:13 +0800

    update react source

--- 11-vue/vue2-demo/src/views/Pro.vue
+++ 11-vue/vue2-demo/src/views/Pro.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="a">
+    <div>
+      <div class="b">
+        hhh
+      </div>
+    </div>
   </div>
 </template>
+<style scoped>
+.a::v-deep .b { 
+  color:red
+}
+</style>
