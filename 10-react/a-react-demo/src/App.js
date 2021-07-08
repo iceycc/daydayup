32caba11ff5b27d3288edf409c555bba68344b2d
iceyang <w@icey.cc>
2021-07-08 08:31:14 +0800

    update

--- 10-react/a-react-demo/src/App.js
+++ 10-react/a-react-demo/src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [num, setNum] = useState(0);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p onClick={()=>setNum(num + 1)}>点击增加</p>
+        <div>{num}</div>
       </header>
     </div>
   );
